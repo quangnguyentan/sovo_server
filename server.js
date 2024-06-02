8bc9be16afe8b7b3e12de8821ea999cd341b3ae4
Nguyen Tan Quang <tanquang1510@gmail.com>
2024-06-02 19:13:27 +0700

    update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 const app = express();
 require("dotenv").config();
 import cors from "cors";
-const PORT = process.env.PORT || 8080;
+const PORT = 8080;
 import initRoutes from "./src/routes";
 import connectDB from "./src/config/connectDB";
 require("./src/passport/index");
@@ -10,9 +10,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: 'https://sovo2.link/',
     methods: ["GET", "PUT", "POST", "DELETE"],
-    credentials: false,
+    credentials: true,
   })
 );
 initRoutes(app);
